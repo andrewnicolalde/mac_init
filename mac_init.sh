8d564ceb3f10c4def7002402689564a3ea328088
Andrew Nicolalde <anicolalde@isomorphiclabs.com>
2024-11-08 18:15:21 +0000

    Add initial mac init script

---
+++ mac_init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+defaults write com.apple.Dock size-immutable -bool true; killall Dock # Prevent dock from being resized (usually accidentally)
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false # Enable key repeat on VSCode (otherwise holding a key down suggests alternate characters)
