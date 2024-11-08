0b206a1c9f74637d36ed03ea417616c6531bb509
Andrew Nicolalde <anicolalde@isomorphiclabs.com>
2024-11-08 18:25:42 +0000

    Add additional

--- mac_init.sh
+++ mac_init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Defaults changes
+# Many of these are from https://notrab.dev/posts/friendly-mac-os-defaults/
 defaults write com.apple.Dock size-immutable -bool true; killall Dock # Prevent dock from being resized (usually accidentally)
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false # Enable key repeat on VSCode (otherwise holding a key down suggests alternate characters)
+defaults write com.apple.screencapture disable-shadow -bool true # No shadows on window screenshots
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true # Don't offer to make every new backup disk a TimeMachine backup drive
 
 # Others
 sudo nvram SystemAudioVolume=" " # Silence boot chime (annoys coworkers)
