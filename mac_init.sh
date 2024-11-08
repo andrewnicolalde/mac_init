1412aa4e04cf40dda0f379501138839760710832
Andrew Nicolalde <anicolalde@isomorphiclabs.com>
2024-11-08 18:18:39 +0000

    Silence startup chime

--- mac_init.sh
+++ mac_init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Defaults changes
 defaults write com.apple.Dock size-immutable -bool true; killall Dock # Prevent dock from being resized (usually accidentally)
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false # Enable key repeat on VSCode (otherwise holding a key down suggests alternate characters)
+
+# Others
+sudo nvram SystemAudioVolume=" " # Silence boot chime (annoys coworkers)
