25a1d4c69c7407526cb8eb7bc1b51c71de30d5b2
Andrew Nicolalde <anicolalde@isomorphiclabs.com>
2024-11-25 11:16:33 +0000

    Change dock hide/show to happen instantly

--- mac_init.sh
+++ mac_init.sh
@@ -6,7 +6,8 @@ defaults write com.apple.Dock size-immutable -bool true; killall Dock # Prevent 
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false # Enable key repeat on VSCode (otherwise holding a key down suggests alternate characters)
 defaults write com.apple.screencapture disable-shadow -bool true # No shadows on window screenshots
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true # Don't offer to make every new backup disk a TimeMachine backup drive
-defaults write com.apple.dock autohide-time-modifier -float 0.15; killall Dock # Remove delay to show hidden dock
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -int 0; killall Dock
 
 # Others
 sudo nvram SystemAudioVolume=" " # Silence boot chime (annoys coworkers)
